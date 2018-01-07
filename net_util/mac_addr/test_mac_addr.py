66dfbeaa1dd3faac30d12df02c9df8b6ec71aa72
Kiminori Kitahata <kitahata@rikinet.co.jp>
2018-01-07 16:19:17 +0900

    ユニットテストを追加した

--- net_util/mac_addr/test_mac_addr.py
+++ net_util/mac_addr/test_mac_addr.py
@@ -8,8 +8,10 @@ class TestMacAddr(TestCase):
         self.assertEqual('0a:0b:0c:0d:0e:0f', to_colon_separated('0A0B0C0D0E0F'))
         self.assertEqual('01:02:03:04:05:06', to_colon_separated('1:2:3:4:5:6'))
         self.assertEqual('00:01:02:03:04:05', to_colon_separated('1.203.0405'))
+        self.assertEqual('00:01:02:03:04:05', to_colon_separated('00-01-02-03-04-05'))
 
     def test_to_dot_separated(self):
         self.assertEqual('0102.0304.0a0b', to_dot_separated('01:02:03:04:0A:0B'))
         self.assertEqual('0a0b.0c0d.0e0f', to_dot_separated('a:b:c:d:e:f'))
         self.assertEqual('0a0b.0c0d.0e0f', to_dot_separated('0a0b0c0d0e0f'))
+        self.assertEqual('0a0b.0c0d.0e0f', to_dot_separated('0a-0b-0c-0d-0e-0f'))
